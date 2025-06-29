fa6899493eb42932b19ffb2da5a8d0581a5eb6fd
BrunoFerMarques <brunofernandesmarques259@gmail.com>
2025-06-29 14:22:44 -0300

    adicionando iluminacao e arrumando foguetes

---
+++ Assets/Scripts/vfx/PositionFireRight.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PositionFireRight : MonoBehaviour
+{
+    public SpaceshipController spaceship;
+    public Vector3 localOffset = new Vector3(1.5f, -1f, -6f);
+
+    void Update()
+    {
+        // Calcula a posição global do fogo a partir do offset local
+        transform.position = spaceship.transform.TransformPoint(localOffset);
+
+        // Copia a rotação da nave
+        transform.rotation = spaceship.transform.rotation;
+    }
+}
